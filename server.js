55736beb2a988137f5258c9552dc3657a1238188
YuanJIU0216 <chli3234@uni.sydney.edu.au>
2023-06-02 21:18:51 +1000

    damn it

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
-})
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+   })
 
-let server = app.listen(8888, function(){
-    console.log('App server is running on port 8888');
-});
+app.listen(8888, function(){
+    console.log("App server is running on port 8888");
+   });
